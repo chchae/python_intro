f36e2eb433434f13a1c69614dafd75b5471f8d2b
chchae <ch1chae@gmail.com>
2024-07-07 17:16:22 +0200

    test

--- thread/t2.py
+++ thread/t2.py
@@ -13,7 +13,6 @@ def is_model_exists( code: str ) -> bool :
     return False
 
 
-
 @dataclass
 class SequenceFasta:
     code : str = ""
@@ -24,6 +23,11 @@ class SequenceFasta:
         return 0 < len(self.code) and 0 < len(self.sequence)
 
 
+
+def fetcher_dummy(_: int | str | None = None) -> Generator[str, None, None] :
+    _seqs : list[int] = [ random.randint( 35, 42 ) for _ in range(100) ]
+    for id in _seqs:
+        yield str(id)
 
 def fetcher_fasta( filename : str ) -> Generator[SequenceFasta, None, None] :
     with open( filename ) as ifp :
@@ -54,10 +58,29 @@ def build_fasta( seq: str, jobid: int = 0 ) -> int :
 
     #print( f"{jobid} : {seq = }")
     # cmd = "python /Users/chchae/bin/rsa-encode.py " + seq
-    cmd = f"echo '{seq}' | shasum -a 512256 | shasum -a 512256"
+    cmd = f"echo '{seq}' | shasum -a 512256 | shasum -a 512256 | shasum -a 512256 | shasum -a 512256"
     ret = subprocess.check_output( cmd, shell=True ).decode("utf-8").strip()
     print( f"{jobid} : {seq = }, {ret =}")
     return jobid
+
+
+def main_functions() -> None :
+    MAX_WORKERS : int = cpu_count()
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        fname = "/Users/chchae/work/data/sequence-short.fasta"
+        fetch_func = partial( fetcher_fasta, filename=fname )
+        build_func = partial( build_fasta )
+        futures : list[Future[int]] = []
+        for id, seq in enumerate( fetch_func() ) :
+            if is_model_exists( seq.code ) :
+                continue
+            futures.append( executor.submit( build_func, seq.sequence, id ) ) 
+        results = [ future.result() for future in as_completed(futures) ]
+        # print(results)
+    print( f"Done {len(results)}-sequences...")
+
+
+
 
 
 
@@ -71,8 +94,11 @@ class SequenceFetcher(ABC) :
 
 
 class DummySequenceFetcher(SequenceFetcher) :
-    def __init__(self, count : int = 100 ) :
-        self._seqs : list[int] = [ random.randint( 30, 42 ) for _ in range(count) ]
+    def __init__(self, length:int = 128, count : int = 100 ) :
+        self._seqs : list[str] = [
+            ''.join( random.choices( "ACDEFGHIKLMPQRSTVWY", k=length ) ) 
+            for _ in range(count)
+        ]
 
     def fetch(self) -> Generator[SequenceFasta, None, None] :
         for id in self._seqs:
@@ -107,15 +133,15 @@ class ProteinBuilder(ABC) :
     jobid : int = 0
 
     @abstractmethod
-    def build( self, seq: str ) -> int :
+    def build( self, seq: str, jobid: int = 0 ) -> int :
         raise NotImplementedError()
 
 
 @dataclass
 class DummyProteinBuilder(ProteinBuilder) :
-    def build( self, seq: str ) -> int :
+    def build( self, seq: str, jobid: int = 0 ) -> int :
         import subprocess
-        nseed = int(seq)
+        nseed = random.randint( 35, 42)
         print( f"start  : {self.jobid = :2d}, {nseed = :2d}" )
         cmd = "/Users/chchae/bin/fibo " + str(nseed)
         ret = subprocess.check_output( cmd, shell=True ).decode("utf-8")
@@ -132,49 +158,33 @@ class ModellerProteinBuilder(ProteinBuilder) :
         return jobid
 
 class RosettaFoldProteinBuilder(ProteinBuilder) :
-    def build( self, seq: str ) -> int :
+    def build( self, seq: str, jobid: int = 0 ) -> int :
         raise NotImplementedError()
 
 class AlphaFoldProteinBuilder(ProteinBuilder) :
-    def build( self, seq: str ) -> int :
+    def build( self, seq: str, jobid: int = 0 ) -> int :
         raise NotImplementedError()
 
-
-
-
-def main_functions() -> None :
-    MAX_WORKERS : int = cpu_count()
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        fname = "/Users/chchae/work/data/sequence-short.fasta"
-        fetch_func = partial( fetcher_fasta, filename=fname )
-        build_func = partial( build_fasta )
-        futures : list[Future[int]] = []
-        for id, seq in enumerate( fetch_func() ) :
-            if is_model_exists( seq.code ) :
-                continue
-            futures.append( executor.submit( build_func, seq.sequence, id ) ) 
-        results = [ future.result() for future in as_completed(futures) ]
-        # print(results)
-    print( f"Done {len(results)}-sequences...")
 
 
 def main() -> None :
     MAX_WORKERS : int = cpu_count()
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        # fetcher = DummySequenceFetcher(300)
+        # fetcher = DummySequenceFetcher(128, 300)
         # builder = DummyProteinBuilder(0)
         fname = "/Users/chchae/work/data/sequence-short.fasta"
         fetcher = FastaSequenceFetcher(fname)
         builder = ModellerProteinBuilder(0)
         futures : list[Future[int]] = []
-        for _, seq in enumerate( fetcher.fetch() ) :
+        for id, seq in enumerate( fetcher.fetch() ) :
             if is_model_exists( seq.code ) :
                 continue
-            futures.append( executor.submit( builder.build, seq.sequence ) ) 
+            futures.append( executor.submit( builder.build, seq.sequence, id ) ) 
         results = [ future.result() for future in as_completed(futures) ]
         # print(results)
     print( f"Done {len(results)}-sequences...")
 
 
 if __name__ == "__main__" :
+    # main_functions()
     main()
