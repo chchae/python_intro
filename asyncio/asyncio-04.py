48522ad34f3fcc2e99f7032f34c9ddf15a44aa64
chchae <ch1chae@gmail.com>
2024-07-09 21:57:27 +0200

    test

--- asyncio/asyncio-04.py
+++ asyncio/asyncio-04.py
@@ -16,9 +16,11 @@ else:
 
 
 type SequenceFasta = tuple[ str, str, str ]
+type FetcherFuncType = AsyncGenerator[SequenceFasta,None]
+type BuilderFuncType = Coroutine[ None, None, list[str] ]
 
 
-async def dummy_sequence_fetch_closure( lock: asyncio.Lock, length: int = 128, count: int = 100 ) -> AsyncGenerator[SequenceFasta,None] :
+async def dummy_sequence_fetch_closure( lock: asyncio.Lock, length: int = 128, count: int = 100 ) -> FetcherFuncType :
     _seqs : list[str] = [
         ''.join( random.choices( "ACDEFGHIKLMPQRSTVWY", k=length ) ) for _ in range(count)
     ]
@@ -31,7 +33,7 @@ async def dummy_sequence_fetch_closure( lock: asyncio.Lock, length: int = 128, c
     return fetch_func()
 
 
-async def fasta_sequence_fetch_closure( filename : str, lock: asyncio.Lock ) -> AsyncGenerator[SequenceFasta,None] :
+async def fasta_sequence_fetch_closure( filename : str, lock: asyncio.Lock ) -> FetcherFuncType :
     async def fetch_func() -> AsyncGenerator[SequenceFasta,None] :
         with open( filename ) as ifp :
             lines = ( x[1] for x in groupby(ifp, lambda line: str(line).startswith(">") ) )
@@ -45,16 +47,16 @@ async def fasta_sequence_fetch_closure( filename : str, lock: asyncio.Lock ) -> 
     return fetch_func()
 
 
-async def oracle_sequence_fetch_closure( url : str, userid : str, passwd : str, lock: asyncio.Lock ) -> AsyncGenerator[SequenceFasta,None] :
-    async def fetch_func() -> AsyncGenerator[SequenceFasta,None] :
+async def oracle_sequence_fetch_closure( url : str, userid : str, passwd : str, lock: asyncio.Lock ) -> FetcherFuncType :
+    async def fetch_func() -> FetcherFuncType :
         raise NotImplementedError()
         yield ( "", "", "")
 
     return fetch_func()
 
 
-async def sqlite_sequence_fetch_closure( url : str, userid : str, passwd : str, lock: asyncio.Lock ) -> AsyncGenerator[SequenceFasta,None] :
-    async def fetch_func() -> AsyncGenerator[SequenceFasta,None] :
+async def sqlite_sequence_fetch_closure( url : str, userid : str, passwd : str, lock: asyncio.Lock ) -> FetcherFuncType :
+    async def fetch_func() -> FetcherFuncType :
         raise NotImplementedError()
         yield ( "", "", "")
 
@@ -72,7 +74,7 @@ def is_valid_sequence_fasta( data: SequenceFasta ) -> bool :
     return 0 < len(code) and 0 < len(sequence)
 
 
-async def modeller_protein_builder_closure( jobid: int, fetcher : AsyncGenerator[SequenceFasta,None] ) -> Coroutine[ None, None, list[int] ] :
+async def dummy_protein_builder_closure( jobid: int, fetcher : FetcherFuncType ) -> BuilderFuncType :
 
     async def build_func() :
         results = [ await _build( fasta ) async for fasta in fetcher ]
@@ -81,11 +83,11 @@ async def modeller_protein_builder_closure( jobid: int, fetcher : AsyncGenerator
 
     async def _build( fasta: SequenceFasta) :
         code, _, seq = fasta
-        print( f"{jobid:2d}: {code}" )
+        print( f"{jobid:2d}: {code} {seq[:40]}" )
         fib = int( await process_command( FIBO_EXE + " " + str( random.randint(38, 45)) ) )
         enc = await process_command( f"echo '{seq}' | shasum -a 512256 | shasum -a 512256 | shasum -a 512256" )
-        print( f"{jobid:2d}: {code} {fib} {enc[:50]}" )
-        return fib
+        print( f"{jobid:2d}: {code} {fib} {enc[:40]}" )
+        return code
 
     async def process_command( cmd: str ) -> str :
         proc = await asyncio.create_subprocess_shell(
@@ -100,14 +102,22 @@ async def modeller_protein_builder_closure( jobid: int, fetcher : AsyncGenerator
     return build_func()
 
 
-async def rosettafold_protein_builder_closure( jobid: int, fetcher : AsyncGenerator[SequenceFasta,None] ) -> Coroutine[ None, None, list[int] ] :
+async def modeller_protein_builder_closure( jobid: int, fetcher : FetcherFuncType ) -> BuilderFuncType :
+
     async def build_func() :
         raise NotImplementedError()
 
     return build_func()
 
 
-async def alphafold_protein_builder_closure( jobid: int, fetcher : AsyncGenerator[SequenceFasta,None] ) -> Coroutine[ None, None, list[int] ] :
+async def rosettafold_protein_builder_closure( jobid: int, fetcher : FetcherFuncType ) -> BuilderFuncType :
+    async def build_func() :
+        raise NotImplementedError()
+
+    return build_func()
+
+
+async def alphafold_protein_builder_closure( jobid: int, fetcher : FetcherFuncType ) -> BuilderFuncType :
     async def build_func() :
         raise NotImplementedError()
 
@@ -121,14 +131,14 @@ async def main() -> None :
     lock = asyncio.Lock()
     # fetcher = await dummy_sequence_fetch_closure(lock)
     fetcher = await fasta_sequence_fetch_closure(FASTA_DATA, lock)
-    builder = modeller_protein_builder_closure
+    builder = dummy_protein_builder_closure
     async with asyncio.TaskGroup() as tg:
         tasks = [ tg.create_task( await builder(id, fetcher) ) for id in range(MAX_WORKERS) ]
     results = [ task.result() for task in tasks ]
     
     results = [ r for rs in results for r in rs ]
-    print( f"{len(results)}-structures : {results}" )
-    print( f"Elapsed time {time.perf_counter() - start:.1f}" )
+    print( f"\n{len(results)}-structures : {results}" )
+    print( f"\nElapsed time {time.perf_counter() - start:.1f}" )
 
 
 if __name__ == "__main__" :
